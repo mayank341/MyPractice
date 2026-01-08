36c8d206f4d8367e288aa60082f2fbd92a373fc2
Mayank Kumar <mkvasudev768@gmail.com>
2026-01-09 04:57:33 +0530

    sum of two numbers :

---
+++ JAVA -CodingSeekho/SumofTwonumbers.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class SumofT100wonumbers{
+
+    public static void main(String args[]){
+        System.out.println("ENter the First Number :");
+
+        Scanner sc=new Scanner(System.in);
+
+        int a=sc.nextInt();
+
+        System.out.println("ENter the second number:");
+        int b=sc.nextInt();
+
+        int sum=a+b;
+
+        System.out.println(sum); 
+
+
+    }
+}
---
+++ JAVA -CodingSeekho/sumoftwonumber.java
@@ -0,0 +1,16 @@
+
+import java.io.*;
+import java.util.*;
+public class sumoftwonumber {
+    public static void main(String args[]){
+        System.out.println(("enter the first number:"));
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println("Enter the Second Numebr :");
+        int b=sc.nextInt();
+
+        int sum=a+b;
+        System.out.println("The sum of two numbers is: " + sum);
+
+    }
+}
